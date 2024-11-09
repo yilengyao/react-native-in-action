497d949e00236690aa1d769ea0028c52e47911de
Yi leng Yao <yilengyao@gmail.com>
2024-11-09 02:13:26 -0800

    border combinations

--- app/App.tsx
+++ app/App.tsx
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 
 export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.cardContainer} />
+        <Example style={{ borderWidth: 1 }}>
+          <Text>borderWidth: 1</Text>
+        </Example>
+        <Example style={{ borderWidth: 3, borderLeftWidth: 0 }}>
+          <Text>borderWidth: 3, borderLeftWidth: 0</Text>
+        </Example>
+        <Example style={{ borderWidth: 3, borderLeftColor: 'red' }}>
+          <Text>borderWidth: 3, borderLeftColor: 'red'</Text>
+        </Example>
+        <Example style={{ borderLeftWidth: 3 }}>
+          <Text>borderLeftWidth: 3</Text>
+        </Example>
+        <Example style={{ borderWidth: 1, borderStyle: 'dashed' }}>
+          <Text>borderWidth: 1, borderStyle: 'dashed'</Text>
+        </Example>
       </View>
     )
   }
 }
 
-const profileCardColor = 'dodgerblue';
+interface ExampleProps {
+  style: any
+  children: any
+}
+
+const Example = (props: ExampleProps) => (
+  <View style={[styles.example, props.style]}>
+    {props.children}
+  </View>
+)
 
 const styles = StyleSheet.create({
   container: {
@@ -20,9 +43,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  cardContainer: {
-    backgroundColor: profileCardColor,
-    width: 300,
-    height: 400
+  example: {
+    marginBottom: 15
   }
 });

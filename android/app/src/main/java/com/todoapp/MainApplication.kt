d4d7897341291bf09f71d42d3d5fb37aae3d6cd7
Yi leng Yao <yilengyao@gmail.com>
2024-11-08 17:13:33 -0800

    rename project

--- android/app/src/main/java/com/todoapp/MainApplication.kt
+++ android/app/src/main/java/com/todoapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.todoapp
+package com.reactnativeinaction
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/ReactNativeInActionTests/ReactNativeInActionTests.m
+++ ios/ReactNativeInActionTests/ReactNativeInActionTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TodoAppTests : XCTestCase
+@interface ReactNativeInActionTests : XCTestCase
 
 @end
 
-@implementation TodoAppTests
+@implementation ReactNativeInActionTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {

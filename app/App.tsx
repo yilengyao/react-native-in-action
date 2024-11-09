5c9e40200e22bbe0352724d5b60c13f7135586a1
Yi leng Yao <yilengyao@gmail.com>
2024-11-09 10:10:53 -0800

    add profile image container and profile image as font awesome

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
-
-export default class App extends Component<{}> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.cardContainer} />
+        <View style={styles.cardContainer}>
+          <View style={styles.cardImageContainer}>
+            <FontAwesomeIcon style={styles.cardImage} icon={faUser} size={50} color="black" />
+          </View>
+        </View>
       </View>
     )
   }
@@ -21,8 +26,25 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   cardContainer: {
+    borderColor: 'black',
+    borderStyle: 'solid',
     backgroundColor: profileCardColor,
     width: 300,
-    height: 400
+    height: 400,
+    borderRadius: 20,
+    borderWidth: 3,
+  },
+  cardImageContainer: {
+    backgroundColor: 'white',
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    borderWidth: 3,
+    borderColor: 'black'
+  },
+  cardImage: {
+    width: 80,
+    height: 80,
+    margin: 10
   }
 });
